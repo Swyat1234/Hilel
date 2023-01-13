4eeabf4a8b56c5c66c864c0af67e3c426495f1e1
Swyat1234 <118475629+Swyat1234@users.noreply.github.com>
2023-01-13 14:35:51 +0300

    Delete Cordinates.java

--- Cordinates.java
+++
@@ -1,9 +0,0 @@
-public class Cordinates {
-    public static void main(String[] args) {
-        double longitude = 10.424543;
-        double latitude = 50.720399 ;
-        System.out.println("My coordinates are: ");
-        System.out.println("latitude: " + latitude);
-        System.out.println("latitude: " + latitude);
-    }
-}
